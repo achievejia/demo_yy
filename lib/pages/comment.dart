1f04506371e3270f121ecf11686a7e37b3eb2369
李嘉 <lij12@shuhaisc.com>
2020-05-05 22:36:01 +0800

    1、添加意见布局

--- lib/pages/comment.dart
+++ lib/pages/comment.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yy_demo/models/article_comment.dart';
 
 class CommentPage extends StatefulWidget {
   final String id;
@@ -20,6 +21,48 @@ class _CommentPageState extends State<CommentPage> {
 
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("全部评论")
+      ),
+      body: Container(
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        child: Stack(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(bottom: 50.0),
+              child: ,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildReplys(List<ArticleReply> replys) {
+    return replys.map((v) => GestureDetector(
+      onTap: () {
+        setState(() {
+          replayName = v.authorNickname;
+        });
+      },
+      child: Container(
+        margin: EdgeInsets.only(bottom: 5.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              "${v.authorNickname}:",
+              style: TextStyle(color: Colors.blue),
+            ),
+            SizedBox(
+              width: 8.0,
+            ),
+            Expanded(child: Text("${v.content}"),)
+          ],
+        ),
+      ),
+    )).toList();
   }
 }
